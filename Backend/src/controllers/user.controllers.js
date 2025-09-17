3fff21f6ff4d49c54234299f3c5b0991d5aea3e4
Yash Libert <yashkulkarni025@gmail.com>
2025-09-17 17:53:53 +0530

    Added Logout Feature for user

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -96,6 +96,30 @@ const logedinUser = asyncHandler(async(req,res) => {
     )
 })
 
+const loggedOut = asyncHandler(async(req,res) =>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out"))
+})
 
 
 
@@ -108,4 +132,5 @@ const logedinUser = asyncHandler(async(req,res) => {
 
 
 
-export {registerUser, logedinUser}
+
+export {registerUser, logedinUser, loggedOut}
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { registerUser, logedinUser } from "../controllers/user.controllers.js"
+import { registerUser, logedinUser, loggedOut } from "../controllers/user.controllers.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js"
 
 const router = Router()
 
 router.route("/register").post(registerUser)
 router.route("/login").post(logedinUser)
+router.route("/logout").post(verifyJWT, loggedOut)
 
 
 
